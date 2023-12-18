b12e8da5ae0049d57d6ba8c9c884bb1e09e89006
DadiSuhendi <dadiart90@gmail.com>
2023-12-18 22:05:47 +0700

    update: accessibility

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="grid xl:grid-cols-3 md:grid-cols-2 md:gap-10 gap-6">
         <div class="bg-white border border-gray-100 shadow-sm w-full rounded-[30px] lg:p-12 p-7">
-            <img src="https://images.unsplash.com/photo-1605772575717-4548a90a5349?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 1" class="w-full h-[400px] rounded-3xl m-auto object-cover">
+            <img src="../assets/project1.jpeg" alt="Project 1" class="w-full h-[400px] rounded-3xl m-auto object-cover">
             <p class="text-2xl font-bold mt-7 text-[#222222]">Lorem Ipsum</p>
             <p class="font-semibold md:text-base text-sm mt-2 uppercase text-[#222222]">First Project</p>
         </div>
         <div class="bg-white border border-gray-100 shadow-sm w-full rounded-[30px] lg:p-12 p-7">
-            <img src="https://images.unsplash.com/photo-1579389083175-247ef703006f?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 2" class="w-full h-[400px]  rounded-3xl m-auto object-cover">
+            <img src="../assets/project2.jpeg" alt="Project 2" class="w-full h-[400px]  rounded-3xl m-auto object-cover">
             <p class="text-2xl font-bold mt-7 text-[#222222]">Lorem Ipsum</p>
             <p class="font-semibold md:text-base text-sm mt-2 uppercase text-[#222222]">Second Project</p>
         </div>
         <div class="bg-white border border-gray-100 shadow-sm w-full rounded-[30px] lg:p-12 p-7">
-            <img src="https://plus.unsplash.com/premium_photo-1661293879952-c5c093282801?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 3" class="w-full h-[400px] rounded-3xl m-auto object-cover">
+            <img src="../assets/project3.jpeg" alt="Project 3" class="w-full h-[400px] rounded-3xl m-auto object-cover">
             <p class="text-2xl font-bold mt-7 text-[#222222]">Lorem Ipsum</p>
             <p class="font-semibold md:text-base text-sm mt-2 uppercase text-[#222222]">Third Project</p>
         </div>
         <div class="bg-white border border-gray-100 shadow-sm w-full rounded-[30px] lg:p-12 p-7">
-            <img src="https://images.unsplash.com/photo-1602757115429-b4190ae087be?q=80&w=2032&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Project 4" class="w-full h-[400px]  rounded-3xl m-auto object-cover">
+            <img src="../assets/project4.jpeg" alt="Project 4" class="w-full h-[400px]  rounded-3xl m-auto object-cover">
             <p class="text-2xl font-bold mt-7 text-[#222222]">Lorem Ipsum</p>
             <p class="font-semibold md:text-base text-sm mt-2 uppercase text-[#222222]">Fourth Project</p>
         </div>
--- src/pages/HomeSection.vue
+++ src/pages/HomeSection.vue
@@ -2,10 +2,10 @@
     <div class="lg:mt-14 mt-10 w-full bg-white rounded-[30px] border border-gray-100 shadow-sm flex flex-col items-center lg:px-[80px] md:px-[60px] px-[20px] lg:py-20 py-10 text-[#222222]" id="home">
         <h1 class="font-bold lg:text-lg md:text-base text-xs uppercase border border-slate-700 px-6 py-3 rounded-full">👋 Hello, I am</h1>
         <p class="lg:text-9xl md:text-6xl text-4xl uppercase font-bold -tracking-widest lg:mt-0 mt-4">Dadi Suhendi</p>
-        <h4 class="mt-4 font-medium lg:text-lg md:text-base text-sm lg:text-center text-justify">Freelance web developer. With a passion for turning visions into visual masterpieces, I specialize in creating websites according to client requests.</h4>
+        <p class="mt-4 font-medium lg:text-lg md:text-base text-sm lg:text-center text-justify">Freelance web developer. With a passion for turning visions into visual masterpieces, I specialize in creating websites according to client requests.</p>
         <a href="#projects" class="mt-8 bg-[#222222] text-white font-bold lg:text-lg md:text-base text-sm uppercase px-6 py-3 rounded-full">👉 Explore My Projects</a>
         <div class="mt-10 w-full">
-            <img src="https://images.unsplash.com/photo-1701505708183-59f709e7d4e6?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="object-cover h-[35rem] w-full rounded-3xl" alt="Profile Image">
+            <img src="../assets/profileImage.jpg" class="object-cover h-[35rem] w-full rounded-3xl" alt="Profile Image">
         </div>
     </div>
 </template>
